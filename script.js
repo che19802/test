6afd2e002259397ae8dd92db3dbb751884564c8b
Andrey <che777@yandex.ru>
2019-11-19 22:06:20 +0700

    Задание 1

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
 'use strict';
 
+// let
+// var
+// const
+/* let objectX = {
+    x1: 1,
+    x2: "str",
+    x3: true
+};   */
+// console.log();
+// alert();
+// confirm();
+// let x = confirm(); 
+// let x = prompt("", "");
+// typeof(x);
+// if () {} else if () {} else {};
+// (условие) ? если условие верно : если условие неверно;
+/* switch (x) {
+    case x < 0:
+        действие
+        break
+    case x > 0:
+        действие
+        break
+    default:
+        действие
+        break
+}   */
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMaried: false
+let money = +prompt("Ваш бюджет в месяц", "0");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "1970-01-01");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false
 };
 
-//console.log(persone["age"]);
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце");
+let answer2 = +prompt("Во сколько обойдется?");
+let answer3 = prompt("Введите обязательную статью расходов в этом месяце");
+let answer4 = +prompt("Во сколько обойдется?");
 
-let arr = ["plum.png", "orenge.jpg", "apple.gif"];
+appData.expenses.answer1 = answer2;
+appData.expenses.answer3 = answer4;
 
-console.log(arr[2]);
+alert("Бюджет на один день = " + (answer2 + answer4)/30);
 
-//alert("Hello World!");
 
-// let answer = confirm('Are you here?');
 
-// console.log(answer);
 
-//let answer = +prompt('Сколько вам лет?', "0");
 
-//console.log(typeof(answer));
 
-//console.log(4 + +"11");
 
-let incr = 10,
-    decr = 10;
-
-console.log(5%2);
-
-console.log ("2" === 2);
