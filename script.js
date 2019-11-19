4b1ccd99ab8133e8a5a8e3a6bdc616c8e4a45c7b
Andrey <che777@yandex.ru>
2019-11-19 19:27:37 +0700

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+
+//console.log(persone["age"]);
+
+let arr = ["plum.png", "orenge.jpg", "apple.gif"];
+
+console.log(arr[2]);
+
+//alert("Hello World!");
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+//let answer = +prompt('Сколько вам лет?', "0");
+
+//console.log(typeof(answer));
+
+//console.log(4 + +"11");
+
+let incr = 10,
+    decr = 10;
+
+console.log(5%2);
+
+console.log ("2" === 2);
